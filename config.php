5f5b83e188d0340022c3df5650c52cffa1b12c04
Neemias Borges <neemias.matos@fatec.sp.gov.br>
2024-09-17 20:09:58 -0300

    docs: adição do arquivo de congifuração

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define( 'DB_NAME',     'clinica' );
+define( 'DB_USER',     'host' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST',     'localhost' );
+define( 'DB_CHARSET',  'utf8' );
+
+$table_prefix = 'tbl';
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
