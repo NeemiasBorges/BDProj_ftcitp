f54b1982908edd49a9d873ad9ef1529e08ba4ab5
Neemias Borges <neemias.matos@fatec.sp.gov.br>
2024-09-17 20:18:46 -0300

    fix: ajustando o usuario do banco

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'DB_NAME',     'clinica' );
-define( 'DB_USER',     'host' );
+define( 'DB_USER',     'root' );
 define( 'DB_PASSWORD', '' );
 define( 'DB_HOST',     'localhost' );
 define( 'DB_CHARSET',  'utf8' );
